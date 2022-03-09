99892d4fa8594adbd363628692dfda93347f43a3
Julie Chu <julieychu@gmail.com>
2022-03-09 01:23:35 -0600

    fix tag error

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,16 +8,16 @@ const Footer = () => {
             <section className="contact-info">
                 <ul>
                     <li>
-                    <a href="https://anthropology.uchicago.edu/people/faculty/julie-chu" target="_blank" rel="noopener noreferrer"/>
-                    <i class="fa-solid fa-chalkboard-user" alt="chalkboard icon"></i>
+                    <a href="https://anthropology.uchicago.edu/people/faculty/julie-chu" target="_blank" rel="noopener noreferrer">
+                    <i class="fa-solid fa-chalkboard-user" alt="chalkboard icon"></i> </a>
                     </li>
                     <li>
-                    <a href="https://github.com/jehulie" target="_blank" rel="noopener noreferrer"/>
-                    <i class="fa-brands fa-github" alt="github icon"></i>
+                    <a href="https://github.com/jehulie" target="_blank" rel="noopener noreferrer">
+                    <i class="fa-brands fa-github" alt="github icon"></i> </a>
                     </li>
                     <li>
-                        <a href="https://www.instagram.com/covidxlogistics/" target="_blank" rel="noopener noreferrer"/>
-                        <i class="fa-solid fa-file-arrow-down" alt="file icon"></i>
+                        <a href="https://www.instagram.com/covidxlogistics/" target="_blank" rel="noopener noreferrer">
+                        <i class="fa-solid fa-file-arrow-down" alt="file icon"></i> </a>
                     </li>
                 </ul>
             </section>
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -4,8 +4,8 @@ import './Resume.module.css'
 const Resume = () => {
     return (
       <section id="resume">
-      <a href='https://drive.google.com/file/d/12sMoHs1Y7hP33GC-tWmo7VLz82YVgr_q/view?usp=sharing' target="_blank" rel="noopener noreferrer"></a>
-      <i class="fa-solid fa-file-arrow-up"></i>
+      <a href='https://drive.google.com/file/d/12sMoHs1Y7hP33GC-tWmo7VLz82YVgr_q/view?usp=sharing' target="_blank" rel="noopener noreferrer">
+      <i class="fa-solid fa-file-arrow-up"></i> </a>
               <h3>Click HERE for my resume.</h3>
         </section>
         );
